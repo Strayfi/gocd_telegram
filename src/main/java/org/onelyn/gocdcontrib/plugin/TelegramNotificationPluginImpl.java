f13c572ce2ba8e78c9546115f441ea860d4d3320
cicd_user <lendjok@mail.ru>
2025-01-29 22:36:35 +0300

    add panic version

--- src/main/java/org/onelyn/gocdcontrib/plugin/TelegramNotificationPluginImpl.java
+++ src/main/java/org/onelyn/gocdcontrib/plugin/TelegramNotificationPluginImpl.java
@@ -17,7 +17,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.onelyn.gocdcontrib.plugin.util.JSONUtils;
-
+import java.time.ZoneOffset;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
